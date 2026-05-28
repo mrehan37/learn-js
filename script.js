0186e75f0e04a6737d5434eb2185e37f7988ea81
Rehan-PC <muhmmadrehan37@gmail.com>
2026-05-28 20:39:46 +0500

    updated folder

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello Friend");
-const a = 1;
-const b = 2;
-console.log(a + b);
-console.warn("This is not one day game you need to practice daily");
-console.error("Keep this constant");
