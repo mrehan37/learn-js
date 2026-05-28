9149d874b4220079ccf350ca43629893364a19bc
Rehan-PC <muhmmadrehan37@gmail.com>
2026-05-28 20:37:35 +0500

    folder structure updated

---
+++ practice-set/practice-set-0/script.js
@@ -0,0 +1,6 @@
+console.log("Hello Friend");
+const a = 1;
+const b = 2;
+console.log(a + b);
+console.warn("This is not one day game you need to practice daily");
+console.error("Keep this constant");
